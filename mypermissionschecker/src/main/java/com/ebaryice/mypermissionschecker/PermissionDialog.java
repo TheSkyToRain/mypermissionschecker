01cf59d2b6feb9788943282d477b3c49038cfa09
TheSkyToRain <953853620@qq.com>
2018-05-11 01:03:58 +0800

    first commit

---
+++ mypermissionschecker/src/main/java/com/ebaryice/mypermissionschecker/PermissionDialog.java
@@ -0,0 +1,79 @@
+package com.ebaryice.mypermissionschecker;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+
+/**
+ * Created by EbaryIce on 2018/5/9/009.
+ */
+
+public class PermissionDialog {
+    private AlertDialog.Builder builder;
+    private Activity activity;
+    private String title;
+    private String message;
+    private static final String PACKAGE_URL_SCHEME = "package:";
+
+    public PermissionDialog(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String getTitle() {
+        if (TextUtils.isEmpty(title)) {
+            return activity.getString(R.string.check_info_title);
+        } else {
+            return title;
+        }
+    }
+
+    private String getMessage() {
+        if (TextUtils.isEmpty(message)) {
+            return activity.getString(R.string.check_info_message);
+        } else {
+            return message;
+        }
+    }
+
+    public void init() {
+        builder = new AlertDialog.Builder(activity);
+        builder.setTitle(getTitle());
+        builder.setMessage(getMessage());
+
+        builder.setNegativeButton(activity.getString(R.string.check_info_exit), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setPositiveButton(activity.getString(R.string.check_info_setting), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startAppSettings();
+            }
+        });
+    }
+
+    public void show() {
+        builder.show();
+    }
+
+    private void startAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse(PACKAGE_URL_SCHEME + activity.getPackageName()));
+        activity.startActivity(intent);
+    }
+}
